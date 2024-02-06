decbd22432b26510ebe91d3e81734979c464eef9
Fabian <acostahf4@gmail.com>
2024-02-05 21:16:44 -0600

    add context

---
+++ src/types/index.ts
@@ -0,0 +1,48 @@
+import { SVGProps } from "react";
+
+export type IconSvgProps = SVGProps<SVGSVGElement> & {
+	size?: number;
+};
+
+export interface Bundle {
+	id: string;
+	title: string;
+	cost: number;
+	value: number;
+	geoLocation: string;
+	quantity: number;
+	receipt: string;
+	ebayLink: string;
+	createdAt: Date;
+}
+
+export type Bundles = Bundle[];
+
+export type Product = {
+	id: string;
+	title: string;
+	cost: number;
+	value: number;
+	geoLoaction: string;
+	recipt: string;
+	createAt: Date;
+	aiTitle: string;
+	aiDescription: string;
+	dealId: string;
+	ebayLink: string;
+};
+
+export type Stats = {
+	totalCost: number;
+	totalCount: number;
+	totalProfit: number;
+	totalValue: number;
+};
+
+export interface AppContextType {
+	bundles: Bundles;
+	stats: Stats;
+	refreshData: () => Promise<void>;
+	isLoading: boolean;
+	user: any;
+}
